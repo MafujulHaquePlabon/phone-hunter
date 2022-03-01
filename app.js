cb90df9d4246451dc619164cf5751e9dfe03c44a
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2022-03-01 22:02:20 +0600

    uses ternary operator phoneDetails section updated

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 const searchPhone=()=>{
     const searchField= document.getElementById("search-field");
     const searchText= searchField.value;
-    // if(searchText== ""){
-    //   const divErrorContainer= document.getElementById("error-result");
-    //   const divE=document.createElement("div");
-    //   divE.innerHTML=`<h4>Result Not Found</h4>`
-    //   divErrorContainer.appendChild(divE);
-    //  }
     searchField.value="";
-    
    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
    .then(res=>res.json())
    .then(data=>displaySearchResult(data.data))
-}
+    }
     const displaySearchResult=data=>{
       console.log(data.length)
       const searchResult=document.getElementById("search-result");
@@ -26,24 +19,22 @@ const searchPhone=()=>{
        else{
         data.forEach(data=>{
           //console.log(data);
-          document.getElementById("error-result").textContent=''
-           const div=document.createElement("div");
-           div.classList.add('col');
-           div.innerHTML=`<div class="card h-100 p-2 ">
-           <img class="w-50 mx-auto" src="${data.image}" class="card-img-top" alt="...">
-           <div class="card-body">
-             <h3 class="card-title">${data.phone_name}</h3>
-             <p class="card-text">Brand: ${ data.phone_name}</p>
-             <button onclick="loadPhoneDetail('${data.slug}')" class="bg-success border-0 px-5 py-3 text-white fw-bolder rounded-pill" >Details</button>
-             </div>
-           </div>
-         </div>`
-          searchResult.appendChild(div);
+     document.getElementById("error-result").textContent=''
+     const div=document.createElement("div");
+     div.classList.add('col');
+     div.innerHTML=`<div class="card h-100 p-2 ">
+          <img class="w-50 mx-auto" src="${data.image}" class="card-img-top" alt="...">
+     <div class="card-body">
+         <h3 class="card-title">${data.phone_name}</h3>
+         <p class="card-text">Brand: ${ data.phone_name}</p>
+         <button onclick="loadPhoneDetail('${data.slug}')" class="bg-success border-0 px-5 py-3 text-white fw-bolder rounded-pill" >Details</button>
+        </div>
+     </div>
+   </div>`
+     searchResult.appendChild(div);
       });
-     
-  };
-       }
-       
+    };
+  }
  const loadPhoneDetail=phoneNameId=>{
    //console.log(phoneNameId)
    fetch(`https://openapi.programming-hero.com/api/phone/${phoneNameId}`)
@@ -51,7 +42,7 @@ const searchPhone=()=>{
    .then(data=>displayPhoneDetail(data.data))
  }
 const displayPhoneDetail=phone=>{
-  console.log(phone.others.USB)
+  //console.log(phone.others.USB)
   const phoneDetails= document.getElementById("phone-details");
   phoneDetails.textContent=""
   const div=document.createElement("div");
@@ -71,15 +62,12 @@ const displayPhoneDetail=phone=>{
   <br>
    <p><span class="fw-bolder">Sensors:</span> ${phone.mainFeatures.sensors}</p>
    <br>
-   <p><span class="fw-bolder">WLAN:</span> ${phone.others.WLAN}</p>
-   <p><span class="fw-bolder">Bluetooth:</span> ${phone.others.Bluetooth}</p>
-   <p><span class="fw-bolder">GPS:</span> ${phone.others.GPS}</p>
-   <p><span class="fw-bolder">NFC:</span> ${phone.others.NFC}</p>
-   <p><span class="fw-bolder">Radio:</span> ${phone.others.Radio}</p>
-   <p><span class="fw-bolder">USB:</span> ${phone.others.USB}</p>
-  </div>
-  <div class="card-body">
-    
+   <p><span class="fw-bolder">WLAN:</span> ${phone?.others?.WLAN ? phone?.others?.WLAN:'not found'}</p>
+   <p><span class="fw-bolder">Bluetooth:</span> ${phone?.others?.Bluetooth?phone?.others?.Bluetooth:'not found'}</p>
+   <p><span class="fw-bolder">GPS:</span> ${phone?.others?.GPS?phone?.others?.GPS:'not found'}</p>
+   <p><span class="fw-bolder">NFC:</span> ${phone?.others?.NFC?phone?.others?.NFC:'not found'}</p>
+   <p><span class="fw-bolder">Radio:</span> ${phone?.others?.Radio?phone?.others?.Radio:'not found'}</p>
+   <p><span class="fw-bolder">USB:</span> ${phone?.others?.USB?phone?.others?.USB:'not found'}</p>
   </div>`
   phoneDetails.appendChild(div);
 }
